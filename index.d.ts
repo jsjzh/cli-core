45af4d0c498172f74aa3885bef030fac0136d934
jinzhehao <jinzhehao@souche.com>
2023-01-16 11:37:52 +0800

    edit index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -215,11 +215,17 @@ export interface CliCommandConfig<O, C, H> {
        * 定时执行任务
        *
        * Seconds: 0-59
+       *
        * Minutes: 0-59
+       *
        * Hours: 0-23
+       *
        * Day of Month: 1-31
+       *
        * Months: 0-11 (Jan-Dec)
+       *
        * Day of Week: 0-6 (Sun-Sat)
+       *
        *
        * @example
        *
