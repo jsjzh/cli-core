ea38d822719d0d5d969079243171b3ac4ec7d590
金哲豪 <jinzhehao@souche.com>
2023-02-05 22:41:10 +0800

    feat: 添加 index.d.ts 待完善

--- index.d.ts
+++ index.d.ts
@@ -2,8 +2,9 @@ import type { CronJob, CronJobParameters } from "cron";
 import type { Command } from "commander";
 import type { Logger } from "winston";
 import type { StdioOptions } from "child_process";
+import type { PromptModule, Question, Answers } from "inquirer";
 
-type AnyObject = { [k: keyof any]: any };
+// type AnyObject = { [k: keyof any]: any };
 
 declare namespace Helper {
   interface TaskItem {
@@ -19,6 +20,85 @@ declare namespace Helper {
     run(): Promise<void>;
   }
 
+  interface IPromptConfig {
+    prefix?: string;
+    suffix?: string;
+    initialAnswers?: Partial<Answers>;
+  }
+
+  class Prompt {
+    promptModule: PromptModule;
+    prompts: Question[];
+    baseConfig: IPromptConfig;
+    initialAnswers: Partial<Answers>;
+    constructor(config: IPromptConfig);
+    addInput(inputConfig: {
+      name: string;
+      message: string;
+      default?: string;
+    }): this;
+    addNumber(numberConfig: {
+      name: string;
+      message: string;
+      default?: number;
+    }): this;
+    addConfirm(confirmConfig: {
+      name: string;
+      message: string;
+      default?: boolean;
+    }): this;
+    addList(listConfig: {
+      name: string;
+      message: string;
+      choices: (
+        | {
+            name: string;
+            value: string;
+          }
+        | string
+      )[];
+      default?: string;
+    }): this;
+    addRawList(rawlistConfig: {
+      name: string;
+      message: string;
+      choices: (
+        | {
+            name: string;
+            value: string;
+          }
+        | string
+      )[];
+      default?: string;
+    }): this;
+    addCheckbox(checkboxConfig: {
+      name: string;
+      message: string;
+      choices: (
+        | {
+            name: string;
+            value: string;
+          }
+        | string
+      )[];
+      default?: string[];
+    }): this;
+    addPassword(passwordConfig: {
+      name: string;
+      message: string;
+      default?: string;
+    }): this;
+    addEditor(editorConfig: {
+      name: string;
+      message: string;
+      default?: string;
+    }): this;
+    execute(callback?: (value: Answers) => void): Promise<void>;
+  }
+
+  const createPrompt: (
+    config?: IPromptConfig,
+  ) => (initialAnswers?: Partial<Answers>) => Prompt;
   const createRunTask: (logger: Logger) => (option: RunTaskConfig) => RunTask;
   const createRunCron: (
     logger: Logger,
@@ -31,322 +111,63 @@ declare namespace Helper {
   const createLogger: (option: { appName: string }) => Logger;
 }
 
-interface IArg {
-  name: string;
+interface IBaseParams {
   description: string;
-  selects?: string[];
-  default?: [any, string];
+  default?: string | [string, string];
+  choices?: string[];
+  optional?: boolean;
+  multiple?: boolean;
+}
+interface IArguments extends IBaseParams {}
+interface IOptions extends IBaseParams {
+  short?: string;
 }
 
-export interface CliCommandConfig<O, C, H> {
-  /**
-   * 定义子 command 指令
-   *
-   * @example
-   *
-   * 如：定义 command 为 start，则可以如下方式触发
-   *
-   * ```shell
-   * cli start
-   * ```
-   */
+export interface CliCommandConfig {
   command: string;
-  /**
-   * 描述子 command 功能
-   */
   description: string;
-  /**
-   * 定义 arguments
-   *
-   * @example
-   *
-   * ```ts
-   * [
-   *   {
-   *     name: "<message>",
-   *     description: "输入 push 的内容",
-   *   },
-   *   {
-   *     name: "[branch]",
-   *     description: "输入分支",
-   *     default: ["master", "默认传入 master"],
-   *     selects: ["master", "develop"],
-   *   },
-   * ]
-   * ```
-   */
-  arguments?: IArg[];
-  /**
-   * 定义 options
-   *
-   * @example
-   *
-   * ```ts
-   * [
-   *   {
-   *     name: "-p, --platform",
-   *     description: "选择平台",
-   *   },
-   *   {
-   *     name: "-G, --no-git",
-   *     description: "不要初始化 git",
-   *   },
-   *   {
-   *     name: "-c, --version <version>",
-   *     description: "输入发布时的版本升级方式",
-   *     selects: ["major", "minor", "patch", "premajor", "preminor", "prepatch"],
-   *     default: ["patch", "默认升级方式为：patch"],
-   *   },
-   *   {
-   *     name: "-d, --dirs [dirs...]",
-   *     description: "[dirs...] 会以贪婪的方式获取 dir",
-   *   },
-   * ]
-   * ```
-   */
-  options?: IArg[];
-  /**
-   * 传入 CliCommand 实例
-   *
-   * @example
-   *
-   * ```ts
-   * {
-   *    commands: [
-   *      new CliCommand({
-   *        command: "git",
-   *        description: "git 命令合集",
-   *        action(props) {
-   *          props.instance.outputHelp()
-   *        },
-   *      }),
-   *    ],
-   *  }
-   * ```
-   */
-  commands?: CliCommand<C, H>[];
-  /**
-   * 传入局部 context
-   *
-   * @example
-   *
-   * ```ts
-   * {
-   *   context() {
-   *     return {
-   *       name: "king",
-   *       age: 18,
-   *     }
-   *   },
-   * }
-   * ```
-   */
-  context?(): C;
-  /**
-   * 传入局部 helper
-   *
-   * @example
-   *
-   * ```ts
-   * {
-   *   helper: {
-   *     getName() {
-   *       return "king"
-   *     },
-   *   },
-   * }
-   * ```
-   */
-  helper?: H;
-  /**
-   * 命令行匹配到之后执行 action
-   */
+  arguments?: { [k: string]: IArguments };
+  options?: { [k: string]: IOptions };
+  commands?: CliCommand[];
+  context?: () => { [k: keyof any]: any };
+  helper?: { [k: keyof any]: any };
   action?: (props: {
-    /**
-     * 若定义了 arguments
-     * 则会以数组的形式传入
-     *
-     * @example
-     *
-     * ```ts
-     * props.args[0]
-     * // hello
-     * ```
-     */
-    args: string | number[];
-    /**
-     * 若定义了 options
-     * 则会以对象的形式传入
-     *
-     * @example
-     *
-     * ```ts
-     * props.opts.message
-     * // hello
-     * ```
-     */
-    opts: Partial<O>;
-    /**
-     * 可以获取到全局 + 局部的 context
-     */
-    context: C;
-    /**
-     * logger 输出工具库
-     *
-     * @remarks
-     * props.logger.info: 会在控制台输出
-     *
-     * props.logger.warn: 会在控制台输出，还会被记录到当前目录下的 logs 文件
-     *
-     * props.logger.error: 会在控制台输出，还会被记录到当前目录下的 logs 文件
-     *
-     * @example
-     *
-     * ```ts
-     * props.logger.info("hello world")
-     * ```
-     */
+    data: { [k: keyof any]: any };
+    context: { [k: keyof any]: any };
     logger: ReturnType<typeof Helper.createLogger>;
-    /**
-     * 工具函数
-     */
     helper: {
-      /**
-       * 定时执行任务
-       *
-       * Seconds: 0-59
-       *
-       * Minutes: 0-59
-       *
-       * Hours: 0-23
-       *
-       * Day of Month: 1-31
-       *
-       * Months: 0-11 (Jan-Dec)
-       *
-       * Day of Week: 0-6 (Sun-Sat)
-       *
-       *
-       * @example
-       *
-       * ```ts
-       * const job = props.helper.runCron({
-       *   cronTime: "* * * * * *",
-       *   onTick() {
-       *     props.logger.info("hello wrold")
-       *   },
-       * })
-       *
-       * job.start()
-       * ```
-       */
+      runPrompt: ReturnType<typeof Helper.createPrompt>;
       runCron: ReturnType<typeof Helper.createRunCron>;
-      /**
-       * 定义同步命令行执行
-       *
-       * @example
-       *
-       * ```ts
-       * const run = props.helper.runCmd(process.cwd())
-       * run("echo hello")
-       * ```
-       */
       runCmd: ReturnType<typeof Helper.createRunCmd>;
-      /**
-       * TODO
-       */
       runTask: ReturnType<typeof Helper.createRunTask>;
       [k: keyof any]: any;
-    } & H;
-    /**
-     * Command 实例
-     */
+    };
     instance: Command;
   }) => void;
 }
 
-export class CliCommand<O = AnyObject, C = AnyObject, H = AnyObject> {
-  constructor(config: CliCommandConfig<O, C, H>);
+export class CliCommand {
+  constructor(config: CliCommandConfig);
 }
 
-export interface CliCoreConfig<C, H> {
-  /**
-   * 定义命令行工具的名称
-   *
-   * @example
-   *
-   * 如：定义 name 为 cli，则可以如下方式触发
-   *
-   * ```shell
-   * cli start xxx
-   * cli run xxx
-   * ```
-   */
+export interface CliCoreConfig {
   name: string;
-  /**
-   * 定义命令行工具的版本
-   */
   version: string;
-  /**
-   * 描述命令行工具功能
-   */
   description: string;
-  /**
-   * 传入 CliCommand 实例
-   *
-   * @example
-   *
-   * ```ts
-   * {
-   *    commands: [
-   *      new CliCommand({
-   *        command: "git",
-   *        description: "git 命令合集",
-   *        action(props) {
-   *          props.instance.outputHelp()
-   *        },
-   *      }),
-   *    ],
-   *  }
-   * ```
-   */
-  commands?: CliCommand<C, H>[];
-  /**
-   * 传入全局 context
-   *
-   * @example
-   *
-   * ```ts
-   * {
-   *   context() {
-   *     return {
-   *       name: "king",
-   *       age: 18,
-   *     }
-   *   },
-   * }
-   * ```
-   */
-  context?(): C;
-  /**
-   * 传入全局 helper
-   *
-   * @example
-   *
-   * ```ts
-   * {
-   *   helper: {
-   *     getName() {
-   *       return "king"
-   *     },
-   *   },
-   * }
-   * ```
-   */
-  helper?: H;
+  commands?: CliCommand[];
+  context?: () => { [k: keyof any]: any };
+  helper?: { [k: keyof any]: any };
 }
 
-export class CliCore<C = AnyObject, H = AnyObject> {
-  constructor(config: CliCoreConfig<C, H>);
+export class CliCore {
+  helper: {
+    logger: ReturnType<typeof Helper.createLogger>;
+    runPrompt: ReturnType<typeof Helper.createPrompt>;
+    runCmd: ReturnType<typeof Helper.createRunCmd>;
+    runCron: ReturnType<typeof Helper.createRunCron>;
+    runTask: ReturnType<typeof Helper.createRunTask>;
+    [k: keyof any]: any;
+  };
+  constructor(config: CliCoreConfig);
   execute(): void;
 }
