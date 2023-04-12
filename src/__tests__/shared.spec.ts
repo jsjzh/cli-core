6246a01b47faa1e1e42785a17fdbbb827d9e94b1
jsjzh <kimimi_king@163.com>
2023-04-12 18:07:22 +0800

    test: 添加测试用例

--- src/__tests__/shared.spec.ts
+++ src/__tests__/shared.spec.ts
@@ -2,13 +2,17 @@ import CronJob, { createCronJob } from "@/shared/cronJob";
 import Prompt, { createPrompt } from "@/shared/prompt";
 import Task, { createTask } from "@/shared/task";
 
+const sum = (a: number, b: number) => a + b;
+
 describe("cron", () => {
   test("cron", () => {
     expect(
       createCronJob({
         cronTime: "* * * * * *",
         onTick() {
-          console.log("hello world");
+          test("run task", () => {
+            expect(sum(1, 2)).toBe(3);
+          });
         },
       }),
     ).toBeInstanceOf(CronJob);
@@ -18,19 +22,65 @@ describe("cron", () => {
 describe("prompt", () => {
   test("prompt", () => {
     expect(createPrompt()).toBeInstanceOf(Prompt);
+
+    const prompt = createPrompt({
+      prefix: "demo ",
+      suffix: " test",
+      initialAnswers: {
+        checkbox: "value",
+      },
+    });
+
+    expect(
+      prompt.addCheckbox({ name: "checkbox", choices: [] }),
+    ).toBeInstanceOf(Prompt);
+
+    expect(prompt.addInput({ name: "addInput" })).toBeInstanceOf(Prompt);
+
+    expect(prompt.addNumber({ name: "addNumber" })).toBeInstanceOf(Prompt);
+
+    expect(prompt.addConfirm({ name: "addConfirm" })).toBeInstanceOf(Prompt);
+
+    expect(
+      prompt.addList({ name: "addList", choices: ["foo"] }),
+    ).toBeInstanceOf(Prompt);
+
+    expect(
+      prompt.addRawList({
+        name: "addRawList",
+        choices: [{ key: "foo", value: { age: 18 }, label: "foo label" }],
+      }),
+    ).toBeInstanceOf(Prompt);
+
+    expect(
+      prompt.addCheckbox({
+        name: "addCheckbox",
+        choices: [{ key: "foo", value: { age: 18 }, label: "foo label" }],
+      }),
+    ).toBeInstanceOf(Prompt);
+
+    expect(prompt.addPassword({ name: "addPassword" })).toBeInstanceOf(Prompt);
+
+    expect(prompt.addEditor({ name: "addEditor" })).toBeInstanceOf(Prompt);
+
+    test.todo("验证命令行输入值", () => {
+      prompt.execute();
+    });
   });
 });
 
 describe("task", () => {
   test("task", async () => {
-    expect(createTask({ showLog: false })).toBeInstanceOf(Task);
+    expect(createTask()).toBeInstanceOf(Task);
 
     await expect(
-      createTask()
+      createTask({ showLog: false })
         .add({
-          title: "demo",
+          title: "demo 1",
           task() {
-            console.log("hello world");
+            test("run task", () => {
+              expect(sum(1, 2)).toBe(3);
+            });
           },
         })
         .execute(),
@@ -42,13 +92,17 @@ describe("task", () => {
           {
             title: "demo 1",
             task() {
-              console.log("hello world");
+              test("run task", () => {
+                expect(sum(1, 2)).toBe(3);
+              });
             },
           },
           {
             title: "demo 2",
             task() {
-              console.log("hello world");
+              test("run task", () => {
+                expect(sum(1, 2)).toBe(3);
+              });
             },
           },
         ])
