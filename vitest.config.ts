3f428e8d4a782c3a7c6935d2b3f3926777fe0ab5
jsjzh <kimimi_king@163.com>
2023-04-12 11:46:59 +0800

    test: 修改 vitest 配置

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,9 @@
-import path from "path";
+import path from "node:path";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    alias: { "@": path.resolve("./src") },
+    globals: true,
+    alias: { src: path.resolve(__dirname, "src") },
   },
 });
